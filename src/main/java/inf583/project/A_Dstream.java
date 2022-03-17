5a53cbf85e1eb227d4ab44ea2d955b5ebbd8d4f2
michael fotso <michael.fotso@alphabody.fr>
2022-03-17 22:52:36 +0100

    A.2 dstream

--- src/main/java/inf583/project/A_Dstream.java
+++ src/main/java/inf583/project/A_Dstream.java
@@ -5,6 +5,9 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.*;
+
+import scala.Tuple2;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.File;
@@ -92,9 +95,58 @@ public class A_Dstream {
 		sc.close();
 	}
 	
+	//static Integer count  = 0;
+	static Tuple2<Double,Integer> avg  = new Tuple2(0.0,0);
+	
+	public static void question2_spark_streaming() throws IOException {
+		avg  = new Tuple2(0.0,0);
+		count = 0;
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("avg_streaming");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+    	sc.setLogLevel("ERROR");
+    	
+    	JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(1));
+    	
+    	JavaDStream<String> stream = local_stream(jssc, inputFile);  // Creating QueueStreaming from local file
+    	
+    	JavaDStream<Integer> Integer_stream =  stream.map(x -> Integer.parseInt(x));
+    	
+    	JavaDStream<Tuple2<Integer,Integer>> avg_stream =  Integer_stream.mapToPair(n -> new Tuple2<>(n,1))
+    			.reduce((a,b) -> new Tuple2<>(a._1 + b._1, a._2 + b._2));
+    	 // Calculating max for each Dstream
+    	
+    	// Calculating the max over all the data has been seen
+        avg_stream.foreachRDD(
+    			x->{
+    				count = count +1;
+    				x.collect().stream().forEach(tuple-> {
+						avg = new Tuple2<>(avg._1+ tuple._1, avg._2 + tuple._2);
+						System.out.println("The avg after "+count + " windows is : "+ new Double(avg._1/avg._2));
+						}
+					);
+    				
+    			}
+    			);
+    	System.out.println("#############################");
+    	System.out.println("Question A.2. (Spark streaming Version)");
+		System.out.println("#############################");
+		
+		jssc.start();
+		try {
+			jssc.awaitTerminationOrTimeout(30000);
+			jssc.stop();
+			System.out.println("The avg after 30 secondes of streaming is: "+ new Double(avg._1/avg._2) );
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		sc.close();
+	}
+	
 	public static void main(String[] args) {
 		try {
 			question1_spark_streaming();
+			question2_spark_streaming();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
