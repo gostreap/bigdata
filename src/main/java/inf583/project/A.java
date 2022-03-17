9597af31350e372f288acedef34174a2bc99ad9e
michael fotso <michael.fotso@alphabody.fr>
2022-03-17 20:22:20 +0100

    dataStream

--- src/main/java/inf583/project/A.java
+++ src/main/java/inf583/project/A.java
@@ -4,8 +4,19 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.rdd.RDD;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.io.BufferedReader;
 
 import scala.Tuple2;
 
@@ -91,11 +102,83 @@ public class A {
 		sc.close();
 	}
 	
+	public static JavaDStream local_stream(JavaStreamingContext jssc,String input) throws IOException {
+		
+		JavaSparkContext jsc = new JavaSparkContext ( jssc .ssc ().sc ());
+		FileReader fr = new FileReader(new File(input));
+		
+		BufferedReader br = new BufferedReader (fr);
+		String line ;
+		int count = 0;
+		ArrayList <String > batch = new ArrayList < String >();
+		Queue < JavaRDD < String >> rdds = new LinkedList < >();
+		while (( line = br.readLine ()) != null ) {
+			count +=1;
+			if( count == 30)
+			{
+				JavaRDD <String > rdd = jsc. parallelize ( batch );
+				rdds . add( rdd);
+				batch = new ArrayList <String >();
+				count = 0;
+				
+				//System.out.println(rdd.first());
+			}
+				batch .add( line );
+		}
+		JavaRDD <String > rdd = jsc. parallelize ( batch );
+		rdds . add( rdd);
+		JavaDStream <String > stream = jssc . queueStream (rdds , true );
+		return stream;
+	}
+	
+	public static void question1_spark_streaming() throws IOException {
+		SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("wordCount");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+    	sc.setLogLevel("ERROR");
+    	
+    	JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(1));
+    	
+    	JavaDStream<String> stream = local_stream(jssc, inputFile);
+    	
+    	JavaDStream<Integer> Integer_stream =  stream.map(x -> Integer.parseInt(x));
+    	
+    	JavaDStream<Integer> max =  Integer_stream.reduce( (value1, value2) -> Math.max(value1, value2));
+    	
+    	//Long max = 
+    	
+    	//concat.print();
+        max.foreachRDD(
+    			x->{
+    				x.collect().stream().forEach(n-> System.out.println("the max for 1's windows is"+n));
+    			}
+    			);
+    	System.out.println("#############################");
+    	System.out.println("Question A.1. (Spark streaming Version)");
+    	//System.out.println("The largest integer is : "+ " "+ ".");
+		System.out.println("#############################");
+		
+		jssc.start();
+		try {
+			jssc.awaitTermination();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		sc.close();
+	}
+	
 	public static void main(String[] args) {
-		question1_spark();
+		/*question1_spark();
 		question2_spark();
 		question3_spark();
-		question4_spark();
+		question4_spark();*/
+		try {
+			question1_spark_streaming();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
